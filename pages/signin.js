f8ac2a388b4d3ed5dcb9924451b48ecfac6d8b41
Davaasuren0501 <davaasuren@alldomain.biz>
2023-11-20 23:44:24 +0800

    add sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -14,8 +14,8 @@ export default function SignUp() {
                 <div className="flex justify-center self-center z-10">
                     <div className="p-12 bg-white-500 mx-auto rounded-2xl w-100 ">
                         <div className="mb-4">
-                        <h3 className="font-semibold text-2xl text-gray-800">Sign Up </h3>
-                        <p className="text-gray-700">Please sign up to your account.</p>
+                        <h3 className="font-semibold text-2xl text-gray-800">Sign In </h3>
+                        <p className="text-gray-700">Please sign in to your account.</p>
                         </div>
                         <div className="space-y-5">
                                     <div className="space-y-2">
@@ -28,25 +28,19 @@ export default function SignUp() {
                             </label>
                             <input className="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="" placeholder="Enter your password" />
                         </div>
-                        <div className="space-y-2">
-                            <label className="mb-5 text-sm font-medium text-gray-700 tracking-wide">
-                                Confirm Password
-                            </label>
-                            <input className="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="" placeholder="Enter your password again" />
-                        </div>
                         <div className="flex items-center justify-between">
                             <div className="flex items-center">
                             </div>
                             <div className="text-sm">
-                                I already have an account
-                                <a href="/signin" className="ml-1 text-orange-500 hover:underline">
-                                    Sign In
+                                I don't have an account
+                                <a href="/signup" className="ml-1 text-orange-500 hover:underline">
+                                Sign Up
                                 </a>
                             </div>
                         </div>
                         <div>
                         <button type="submit" className="w-full flex justify-center bg-orange-500  text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
-                            Sign Up
+                            Sign in
                         </button>
                         </div>
                         </div>
